65a883de87b11770a30120cc2f4142f828a4d40e
ArtemKudryavtsev <himynameisartem@icloud.com>
2025-06-24 17:01:03 +0300

    intial commit

---
+++ HabbitTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HabbitTracker
+//
+//  Created by Артем Кудрявцев on 24.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
